368c7f7a5551aecb8987b27813a2c5f34dbbc673
Abin Simon <abinsimon10@gmail.com>
2019-10-28 20:27:25 +0530

    add animated transition

--- src/blob.js
+++ src/blob.js
@@ -1,3 +1,10 @@
+import { Easing, Tween, autoPlay } from "es6-tween";
+
+autoPlay(true);
+const markers = false
+
+let prevPoints = null;
+
 function create(root) {
   const canvas = document.createElement("canvas");
   canvas.setAttribute("touch-action", "none");
@@ -18,14 +25,25 @@ function getRandomArbitrary(min, max) {
   return sign * amount;
 }
 
+function rint(min, max) {
+  const amount = Math.random() * (max - min) + min;
+  return Math.round(amount);
+}
+
 function getCirclePoints(count, base, radius) {
   // const angles = [0, 45, 90, 135, 180, 225, 270, 315]; // randomize gaps (360 omitted)
-  const angles = [0, 90, 180, 270]; // randomize gaps (360 omitted)
+  let angles = [0, 90, 180, 270]; // randomize gaps (360 omitted)
+  angles = [
+    rint(0, 90 - 45),
+    rint(90, 180 - 45),
+    rint(180, 270 - 45),
+    rint(270, 360 - 45)
+  ];
   const positions = [];
-  for (a in angles) {
+  for (let a in angles) {
     const angle = (angles[a] * Math.PI) / 180;
     let ba = ((angles[a] - 20) * Math.PI) / 180;
-    let rr = radius + getRandomArbitrary(40, 80);
+    let rr = radius + getRandomArbitrary(40, 100);
     positions.push({
       x: base.x + radius * Math.sin(angle),
       y: base.y + radius * Math.cos(angle),
@@ -37,35 +55,52 @@ function getCirclePoints(count, base, radius) {
   return positions;
 }
 
-function drawBlob(ctx) {
-  let points = getCirclePoints(null, { x: 300, y: 300 }, 150);
+function drawPath(ctx, points) {
   let cpath = `M${points[0].x},${points[1].y}`;
   for (let point of points)
     cpath += `S${point.mx},${point.my},${point.x},${point.y}`;
   cpath += "Z";
-
   let p = new Path2D(cpath);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "rgb(229, 244, 216)";
   ctx.fill(p);
 
-  // ctx.beginPath();
-  // for (p of points) {
-  //   ctx.arc(p.x, p.y, 2, 0, 2 * Math.PI);
-  // }
-  // ctx.strokeStyle = "red";
-  // ctx.stroke();
-  // ctx.beginPath();
-  // ctx.strokeStyle = "green";
-  // for (p of points) {
-  //   ctx.arc(p.mx, p.my, 2, 0, 2 * Math.PI);
-  // }
-  // ctx.stroke();
+  if (markers) {
+    ctx.beginPath();
+    for (let p of points) {
+      ctx.arc(p.x, p.y, 2, 0, 2 * Math.PI);
+    }
+    ctx.strokeStyle = "red";
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.strokeStyle = "green";
+    for (let p of points) {
+      ctx.arc(p.mx, p.my, 2, 0, 2 * Math.PI);
+    }
+    ctx.stroke();
+  }
+}
+
+function drawBlob(ctx) {
+  let points = getCirclePoints(null, { x: 300, y: 300 }, 150);
+  if (prevPoints === null) {
+    drawPath(ctx, points);
+  } else {
+    let coords = [...prevPoints];
+    let tween = new Tween(coords)
+      .to(points, 1000)
+      .on("update", p => {
+        drawPath(ctx, p);
+      })
+      .start();
+  }
+  prevPoints = points;
 }
 
 const canvas = create(document.body);
 const ctx = canvas.getContext("2d");
 
+drawBlob(ctx);
 setInterval(() => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBlob(ctx);
-}, 1000);
+}, 2000);
